fca96443873dbdbcff8826f3f29d75d2d3c310fa
KingAndrew801 <111556315+KingAndrew801@users.noreply.github.com>
2022-10-09 17:20:02 -0600

    changed prodcut class

--- product.py
+++ product.py
@@ -1,19 +1,23 @@
 from sqlalchemy import create_engine, Column, Float, Integer, Date, String
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
-engine = create_engine('sqlite3:///inventory.db', echo=True)
+engine = create_engine('sqlite3:///inventory.db', echo=False)
+Session = sessionmaker(bind=engine)
+session = Session()
 Base = declarative_base()
 
 class Product(Base):
-    if __name__ == '__main__':
-        product_id = Column(Integer, primary_key=True)
-        product_name = Column(String)
-        product_quantity = Column(Integer)
-        product_price = Column(Float)
-        date_updated = Column(Date)
+    __tablename__ = 'Products'
 
-        def __repr__(self):
-            return f'{self.product_name}, Qty= {self.product_quantity}, Price= {self.product_price}, Update= {self.date_updated}'
+    product_id = Column(Integer, primary_key=True)
+    product_name = Column(String)
+    product_quantity = Column(Integer)
+    product_price = Column(Float)
+    date_updated = Column(Date)
+
+    def __repr__(self):
+        return f'{self.product_name}, Qty= {self.product_quantity}, Price= {self.product_price}, Update= {self.date_updated}'
 
 if __name__ == '__main__':
     Base.metadata.create_all(engine)
