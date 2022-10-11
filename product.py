bd057d26ca7b234a1bc156c9ee51ea7f4eca8937
KingAndrew801 <111556315+KingAndrew801@users.noreply.github.com>
2022-10-10 21:38:14 -0600

    added clean function

--- product.py
+++ product.py
@@ -1,8 +1,9 @@
 from sqlalchemy import create_engine, Column, Float, Integer, Date, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import csv
 
-engine = create_engine('sqlite3:///inventory.db', echo=False)
+engine = create_engine('sqlite:///inventory.db', echo=False)
 Session = sessionmaker(bind=engine)
 session = Session()
 Base = declarative_base()
@@ -19,5 +20,25 @@ class Product(Base):
     def __repr__(self):
         return f'{self.product_name}, Qty= {self.product_quantity}, Price= {self.product_price}, Update= {self.date_updated}'
 
+def cleansheet():
+    with open('store-inventory/inventory.csv', newline='') as csvfile:
+        reader = csv.reader(csvfile, delimiter= '|')
+        rows = list(reader)
+        csvlist = []
+        for row in rows:
+            newnie = row[0].split(',')
+            if len(newnie) == 5:
+                newnie[0] = newnie[0] + "," + newnie[1]
+                newnie.remove(newnie[1])
+                print(newnie)
+            csvlist.append({'product': newnie[0], 'price': newnie[1], 'quantity': newnie[2], 'date': newnie[3]})
+        for item in csvlist:
+            try:
+                item['price'] = int(item['price'])
+            except ValueError:
+                del item
+        print(csvlist)
+
 if __name__ == '__main__':
-    Base.metadata.create_all(engine)
+    # Base.metadata.create_all(engine)
+    cleansheet()
